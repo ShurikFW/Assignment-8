6c690042dd8836eaa09aae495c78ac3f0beaa27f
ShurikFW <109271666+ShurikFW@users.noreply.github.com>
2022-11-16 21:29:43 +0200

    Add files via upload

---
+++ assignment 8/assignment 8/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace assignment_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the boy\'s name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the girl\'s name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(396, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(396, 218);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check name";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ assignment 8/assignment 8/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace assignment_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                StreamReader boys;
+                StreamReader girls;
+                string girlNames;
+                string boyNames;
+                boys = File.OpenText("boys.txt");
+                List<string> boyNameList = new List<string>();
+                girls = File.OpenText("girls.txt");
+                List<string> girlNameList = new List<string>();
+                boyNames = textBox1.Text;
+                girlNames = textBox2.Text;
+                while (!boys.EndOfStream)
+                {
+                    boyNameList.Add(boys.ReadLine());
+
+                }
+                while (!girls.EndOfStream)
+                {
+                    girlNameList.Add(girls.ReadLine());
+
+                }
+                int boysPosition = boyNameList.IndexOf(boyNames);
+                int girlsPosition = girlNameList.IndexOf(girlNames);
+
+                if (boysPosition != -1)
+                {
+                    MessageBox.Show("The boy name you entered is one of the most popular names.");
+
+                }
+
+                if (girlsPosition != -1)
+                {
+                    MessageBox.Show("The girl name you entered is one of the most popular names.");
+
+                }
+                
+
+                if (boysPosition != -1 && girlsPosition != -1)
+                {
+                    MessageBox.Show("The name you entered was found on both the boys and girls name lists!");
+                }
+                
+
+                else if (boysPosition == -1)
+                {
+                    MessageBox.Show("The boy name you have entered is NOT one of the most popular names.");
+
+                }
+
+                else if (girlsPosition == -1)
+                {
+                    MessageBox.Show("The girl name you have entered is NOT one of the most popular names.");
+
+                }
+            }
+         
+            catch (Exception ex)
+            {
+            
+            MessageBox.Show(ex.Message);
+
+            } 
+
+ 
+        }
+    }
+}
